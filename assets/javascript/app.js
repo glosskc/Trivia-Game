6db0aefaa9b6826c664ff8023bbb4eebc33a3453
glosskc <glosskc@gmail.com>
2019-03-25 12:14:55 -0500

    updated site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,8 @@ $("#remaining-time").hide();
 $("#start").on("click", trivia.startGame);
 $(document).on("click", ".option", trivia.guessChecker);
 
+})
+
 var trivia = {
     correct: 0,
     incorrect: 0,
@@ -17,42 +19,42 @@ var trivia = {
     //questions, answers, and choices array
 
     question: {
-        q1:"",
-        q2:"",
-        q3:"",
-        q4:"",
-        q5:"",
-        q6:"",
-        q7:"",
-        q8:"",
-        q9:"",
-        q10:"",
+        q1:"What is the most populated city (city proper) in the world?",
+        q2:"What is the most populated metropolitan area in the world",
+        q3:"What country produces the most coffee per year",
+        q4:"What is the second highest mountain in the world called?",
+        q5:"What city is considered the 'Snowiest Place on Earth'?",
+        q6:"Which country has no national debt?",
+        q7:"What is the most spoken language in the world?",
+        q8:"Which country has the second largest music industry (US is first of course)?",
+        q9:"In which country is the deepest man made hole(40,230ft deep)?",
+        q10:"Which country has the oldest continuously run company?(1400 years old)",
     },
 
     options: {
-        q1:[],
-        q2:[],
-        q3:[],
-        q4:[],
-        q5:[],
-        q6:[],
-        q7:[],
-        q8:[],
-        q9:[],
-        q10:[],
+        q1:["Tokyo", "Shanghai", "Mexico City", "Beijing", "Karachi"],
+        q2:["Mumbai(Bombay)", "New York City", "Beijing", "Shanghai", "Tokyo"],
+        q3:["Vietnam", "Columbia", "Brazil", "Indonesia", "Hawaii"],
+        q4:["Mount Fuji", "Denali", "K2", "Mount Kilimanjaro", "Mount Kangchenjunga"],
+        q5:["Rochester, New York", "Aomori City, Japan", "Anchorage, Alaska", "Denver, Colorado", "Nagano, Japan"],
+        q6:["Switzerland", "Lichtenstein", "Saudi Arabia", "Russia", "UAE"],
+        q7:["Chinese", "Spanish", "English", "Hindi", "Arabic"],
+        q8:["Germany", "The UK", "South Korea", "Japan", "France"],
+        q9:["Russia", "US", "Sweden", "South Africa", "Antartica"],
+        q10:["China", "India", "Japan", "Saudi Arabia", "The UK"],
     },
 
     answers: {
-        q1:"",
-        q2:"",
-        q3:"",
-        q4:"",
-        q5:"",
-        q6:"",
-        q7:"",
-        q8:"",
-        q9:"",
-        q10:"",
+        q1:"Shanghai",
+        q2:"Tokyo",
+        q3:"Brazil",
+        q4:"K2",
+        q5:"Aomori City, Japan",
+        q6:"Lichtenstein",
+        q7:"Chinese",
+        q8:"Japan",
+        q9:"Russia",
+        q10:"Japan",
     },
 
 
@@ -78,7 +80,7 @@ var trivia = {
 
   nextQuestion: function(){
     trivia.timer = 10;
-    $("#timer").removeaClass('last-seconds');
+    $("#timer").removeClass('last-seconds');
     $("#timer").text(trivia.timer);
 
     if(!trivia.timerOn){
@@ -116,8 +118,53 @@ var trivia = {
    }
 
 
-   else if
-}
+   else if(trivia.currentSet === Object.keys(trivia.question).length){
+       $("#results").html("<h3>Good Job! Thanks for Playing!</h3>"+
+       "<p>Correct: "+trivia.correct +"</p>"+
+       "<P>Incorrect: "+ trivia.incorrect +"</p>"+
+       "<p>Unaswered: "+ trivia.unanswered +"</p>"+
+       "<p>Try Again!</p>");
+
+       $("#game").hide();
+
+       $("#start").show();
+   }
+},
+
+    guessChecker: function(){
+        var resultId;
+
+        var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+
+        if($(this).text() === currentAnswer){
+
+            $(this).addClass("btn-success").removeClass("btn-info");
+
+            trivia.correct++;
+            clearInterval(trivia.timeId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $("#results").html("<h3>Correct Answer!</h3>");
+        }
+        else{
+            $(this).addClass("btn-danger").removeClass("btn-info");
+
+            trivia.incorrect++;
+            clearInterval(trivia.timeId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $("#results").html("<h3>Sorry, better try again! "+ currentAnswer +"</h3>");
+
+        }
+    },
+
+    guessResult: function(){
+        trivia.currentSet++;
+
+        $(".option").remove();
+        $("#results h3").remove();
+
+        trivia.nextQuestion();
+    }
+};
 
 
 
@@ -131,4 +178,3 @@ var trivia = {
 //question, answer, and chocies array
 //dynamically push questions and choices to DOM 
 //
-});
